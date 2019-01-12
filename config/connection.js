24ff0fc355455ba9c5558b1da7b5aef05222c46c
Vivien <vchang103@gmail.com>
2019-01-12 21:50:20 +0800

    adding sql to heroku

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-	host: process.env.DB_HOST,
+	host: process.env.DB_HOST || "us-cdbr-iron-east-01.cleardb.net",
 	port: 3306,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASS,
-	database: "burger_db"
+	user: process.env.DB_USER || "b36e8f9ccfd3f5",
+	password: process.env.DB_PASS || "2c89c975",
+	database: "burger_db" || "heroku_5fbc5a737b78440"
 });
 
 connection.connect(function(err) {
